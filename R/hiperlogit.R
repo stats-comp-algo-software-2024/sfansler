67edfed844ca24e13b39652fdd73a4ceff9b5d63
Sam Fansler <sfansle1@jh.edu>
2024-02-01 22:04:47 -0500

    Create R files for function creation and output

---
+++ R/hiperlogit.R
@@ -0,0 +1,6 @@
+hiper_logit <- function(outcome, design){
+  #TODO: Maximize likelihood
+  hlogit <-  list()
+  class(hlogit) <- "hlogit"
+  return(hlogit)
+}
---
+++ R/model_output.R
@@ -0,0 +1,11 @@
+coef.hlogit <- function(hlogit){
+  #Return coefficients
+}
+
+vcov.hlogit <- function(hlogit){
+  #Return variance-covariance matrix
+}
+
+print.hlogit <- function(hlogit){
+  print("Output of hiper_logit()")
+}
