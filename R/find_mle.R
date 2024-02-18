73fec46f28a1fd4231758b5e6261f206c0719a40
Sam Fansler <sfansle1@jh.edu>
2024-02-18 15:13:45 -0500

    Implement logistic model mle finders, fix Newton method

--- R/find_mle.R
+++ R/find_mle.R
@@ -1,29 +1,57 @@
 #' Finds MLE using pseudo_inverse
 find_mle_pseudo_inv = function(design, outcome){
-mle = solve(t(design) %*% design, t(design) %*% outcome)
+mle = list()
+mle$coef = solve(t(design) %*% design, t(design) %*% outcome)
+noise_variance <- mean((outcome - design %*% mle$coef)^2)
+n <- nrow(design); p <- ncol(design)
+noise_variance <- noise_variance / (1 - p / n)
+mle$info_mat = (t(design) %*% design) / noise_variance
 return(mle)
 }
 
-#' Calculates the log-likelihood used to find MLE using BFGS
-log_likelihood = function(beta, outcome, design, noise_variance = 1){
-  n = length(outcome)
-  mu = design %*% beta
-  ll = -1/2 * n * log(2*pi*noise_variance) -1/2 * sum((outcome - mu)^2 / noise_variance)
-  return(ll)
-}
-
-#' Calculates the gradient used to find MLE using BFGS
-gradient = function(outcome, design, beta, noise_variance = 1){
-  n = length(outcome)
-  mu = design %*% beta
-  grad = t(design) %*% (outcome - mu) / noise_variance
-  return(grad)
-}
-
-#'Find MLE using BFGS (via log-likelihood and gradient)
-find_mle_BFGS = function(design, outcome) {
+#'Find MLE using BFGS for a linear model (via log-likelihood and gradient)
+find_mle_optim_linear = function(design, outcome, method) {
   p = ncol(design)
   init = rep(0, p)
-  mle = stats::optim(par = init, fn = log_likelihood, gr = gradient, control = list(fnscale = -1), design = design, outcome = outcome, method = "BFGS")
+  mle = list()
+  mle$coef = stats::optim(par = init, fn = log_likelihood_linear, gr = gradient_linear, control = list(fnscale = -1), design = design, outcome = outcome, method = method)$par
+  mle$info_mat = -hessian(beta = mle$coef, design = design, outcome = outcome)
   return(mle)
 }
+
+find_mle_optim_logistic = function(design, outcome, method) {
+  p = ncol(design)
+  init = rep(0, p)
+  mle = list()
+  mle$coef = stats::optim(par = init, fn = log_likelihood_logistic, gr = gradient_logistic, control = list(fnscale = -1), design = design, outcome = outcome, method = method)$par
+  mle$info_mat = -hessian(beta = mle$coef, design = design, outcome = outcome)
+  return(mle)
+}
+
+find_mle_newton = function(design, outcome) {
+  p = ncol(design)
+  beta = rep(0, p)
+  for(i in 1:500){
+    grad = gradient_logistic(design = design, outcome = outcome, beta = beta)
+    info = -hessian(design = design, outcome = outcome, beta = beta)
+    beta = beta + solve(info, grad)
+    i = i + 1
+  }
+  mle = list(coef = beta, info_mat = info)
+  return(mle)
+}
+
+
+find_mle = function(design, outcome, model, option){
+  if(is.null(option$mle_solver)){
+    if(model == "linear"){
+      mle = find_mle_pseudo_inv(design = design, outcome = outcome)
+    } else {
+      mle = find_mle_newton(design = design, outcome = outcome)
+    }
+  } else {
+    mle = find_mle_optim_logistic(design = design, outcome = outcome, option$mle_solver)
+  }
+  return(mle)
+}
+
--- R/hiperglm.R
+++ R/hiperglm.R
@@ -1,8 +1,14 @@
 #' @export
-hiper_glm <- function(outcome, design){
-  mle = find_mle_BFGS(design, outcome)
-  #To Do: get standard errors for MLE
-  hglm <-  list()
-  class(hglm) <- "hglm"
-  warning("function not implemented yet")
+hiper_glm <- function(outcome, design, model = "linear", option = list()){
+  mle = list()
+
+  if(!(model %in% c("linear", "logit"))){
+    stop("argument 'model' must be 'linear' or 'logit'")
+  }
+
+  mle = find_mle(design = design, outcome = outcome, model = model, option)
+  hglm_out <-  list(coef = mle$coef, info_mat = mle$info_mat)
+
+  class(hglm_out) <- "hglm"
+  return(hglm_out)
 }
